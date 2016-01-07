7ea4b86a55b8f926b569d7567dc16bc16046c795
Arthur Barros <arthurpdb8@gmail.com>
2016-01-07 15:19:41 -0300

    Modifica Database para corrigir conflito

--- banco_novo.sql
+++ banco_novo.sql
@@ -38,15 +38,7 @@ create table componentes(
   descricao varchar(500) not null,
   linha int not null,
   coluna int not null,
-<<<<<<< HEAD
-<<<<<<< HEAD
   quantidade int not null,
-=======
-  quantidade int not null,
->>>>>>> origin/master
-=======
-  quantidade int not null,
->>>>>>> origin/master
 PRIMARY KEY (id)
 );
 
@@ -88,18 +80,6 @@ alter table `emprestimos` add constraint `fk_e_user` foreign key (`user_id`) ref
 
 alter table `emprestimos` add constraint `fk_e_laboratorio` foreign key (`laboratorio_id`) references `laboratorios` (`id`);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 alter table `notifications` add constraint `fk_n_emprestimo` foreign key (`emprestimo_id`) references `emprestimos` (`id`);
 
 alter table `notifications` add constraint `fk_n_componente` foreign key (`componente_id`) references `componentes` (`id`);
-=======
-alter table `notifications` add constraint `fk_n_emprestimo` foreign key (`emprestimo_id`) references `emprestimos` (`id`);
-
-alter table `notifications` add constraint `fk_n_componente` foreign key (`componente_id`) references `componentes` (`id`);
->>>>>>> origin/master
-=======
-alter table `notifications` add constraint `fk_n_emprestimo` foreign key (`emprestimo_id`) references `emprestimos` (`id`);
-
-alter table `notifications` add constraint `fk_n_componente` foreign key (`componente_id`) references `componentes` (`id`);
->>>>>>> origin/master
